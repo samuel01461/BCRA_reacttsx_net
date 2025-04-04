2085d01caf94801ea6814da1575825d2b4722d7d
samuel01461 <samuelgithub5@gmail.com>
2025-04-04 19:24:24 -0300

    Agregamos cadena de conexion sql server a program.cs

--- api_bcra/api_bcra/api_bcra/Program.cs
+++ api_bcra/api_bcra/api_bcra/Program.cs
@@ -16,42 +16,6 @@ builder.Services.AddDbContext<BCRADbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Production"));
 });
 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
-{
-    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
-
-var jwt_config = builder.Configuration.GetSection("JWT_Config");
-var jwt_key = jwt_config.GetValue<string>("SigningKey");
-var jwt_audience = jwt_config.GetValue<string>("Audience");
-var jwt_issuer = jwt_config.GetValue<string>("Issuer");
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateIssuerSigningKey = true,
-        ValidateLifetime = true,
-        ValidAudience = jwt_audience,
-        ValidIssuer = jwt_issuer,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt_key))
-    };
-});
-
-builder.Services.AddCors(p =>
-{
-    p.AddPolicy("mainPolicy", p =>
-    {
-        p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
-});
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
