a6cca85e4615d14b2fb02bdd7b3203c409579677
samuel01461 <samuelgithub5@gmail.com>
2025-04-04 19:40:35 -0300

    Modificamos Program.cs, agregamos evitar ciclos y config basica JWT

--- api_bcra/api_bcra/api_bcra/Program.cs
+++ api_bcra/api_bcra/api_bcra/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using api_bcra.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -16,6 +17,42 @@ builder.Services.AddDbContext<BCRADbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Production"));
 });
 
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+var jwt_config = builder.Configuration.GetSection("JWT_Config");
+var jwt_issuer = jwt_config.GetValue<string>("Issuer");
+var jwt_audience = jwt_config.GetValue<string>("Audience");
+var jwt_signingkey = jwt_config.GetValue<string>("SigningKey");
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwt =>
+{
+    jwt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateActor = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwt_issuer,
+        ValidAudience = jwt_audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt_signingkey))
+    };
+});
+
+builder.Services.AddCors(cors =>
+{
+    cors.AddPolicy("CorsApi", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -29,7 +66,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseCors("mainPolicy");
+app.UseCors("CorsApi");
 
 app.MapControllers();
 
